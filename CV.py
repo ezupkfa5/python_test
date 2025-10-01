fdcc98d2900d0ee62b0a63b3aa9c17fb2a716997
GMH <ezupkfa5@gmail.com>
2025-10-02 00:25:11 +0800

    first step

--- CV.py
+++ CV.py
@@ -1,1 +1,10 @@
-import openai
+import os
+import numpy as np
+import pandas as pd
+#import tensorflow as tf
+
+print(os.getcwd())  # 目前工作目錄
+print(__file__)     # 目前檔案完整路徑
+
+x_train = pd.read_csv("train.csv",index_col="label")
+print(x_train.head())
