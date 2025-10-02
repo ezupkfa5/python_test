d3da9a535e3b0cf21f2a5ed94fed8d6c4641f6e4
GMH <ezupkfa5@gmail.com>
2025-10-03 07:38:47 +0800

    資料集分好

--- CV.py
+++ CV.py
@@ -1,10 +1,19 @@
 import os
-import numpy as np
 import pandas as pd
 #import tensorflow as tf
 
-print(os.getcwd())  # 目前工作目錄
-print(__file__)     # 目前檔案完整路徑
+#print(os.getcwd())  # 目前工作目錄
+#print(__file__)     # 目前檔案完整路徑
 
-x_train = pd.read_csv("train.csv",index_col="label")
-print(x_train.head())
+mnist_train = pd.read_csv("train.csv")
+mnist_test = pd.read_csv("test.csv")
+#print(mnist_test.head())
+
+X_train = mnist_train.drop("label",axis= 1)
+y_train = mnist_train["label"]
+#print(X_train.shape) #(42000, 784))
+
+#print(y_train.shape) #(42000,)
+X_test = mnist_test
+#print(X_test.shape) #(28000, 784)
+
